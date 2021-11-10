66b58c8c78511d1de29fff038eb03495b13de47f
SalomaoFerreira <salomao1991@gmail.com>
2021-11-10 11:01:50 -0500

    testing the return type of methods and add gitignore

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            //NotFound object
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //NotFound object or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        public void GetCustomer_IdIsNotZero_RetornOk()
+        {
+
+        }
+    }
+}
