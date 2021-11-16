8ff1055b5072ddf5cd2534d2e4ea98e0bbbf0671
SalomaoFerreira <salomao1991@gmail.com>
2021-11-16 16:56:44 -0500

    refactoring towards a loosely-coupled design - mock object

---
+++ TestNinja/Mocking/FileReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public interface IFileReader
+    {
+        string Read(string path);
+    }
+
+    // it is the real implementation that uses an external resource.
+    public class FileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
