e02e331bcdcb06fe81c413bb2ee2156c6f08181e
SalomaoFerreira <salomao1991@gmail.com>
2021-11-19 15:38:29 -0500

    an example to dont use mock object. Without external resourcses

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        // use mocks to remove external resources from our unit tests
+        // this class has only internal business rules, so it is better to unit test in a simpler way without mock.
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product() { ListPrice = 100 };
+
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
